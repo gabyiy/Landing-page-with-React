091a3b6ed12715b0e31d8cbeae49d532f1932d34
gabyiy <gabyiy2000@hotmail.com>
2021-07-16 16:25:15 +0000

    second

--- src/js/component/cards.js
+++ src/js/component/cards.js
@@ -2,20 +2,52 @@ import React from "react";
 
 export const Cards = () => {
 	return (
-		<div className="card">
-			<img className="card-img-top" src="..." alt="Card image cap" />
-			<div className="card-body">
-				<h5 className="card-title">Card title</h5>
-				<p className="card-text">
-					This is a longer card with supporting text below as a
-					natural lead-in to additional content. This content is a
-					little bit longer.
-				</p>
-				<p className="card-text">
-					<a href="#" className="btn btn-primary">
+		<div class="card-deck">
+			<div class="card">
+				<img class="card-img-top" src="..." alt="Card image cap" />
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">
+						This is a wider card with supporting text below as a
+						natural lead-in to additional content. This content is a
+						little bit longer.
+					</p>
+				</div>
+				<div class="card-footer">
+					<a href="#" class="btn btn-primary">
 						Go somewhere
 					</a>
-				</p>
+				</div>
+			</div>
+			<div class="card">
+				<img class="card-img-top" src="..." alt="Card image cap" />
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">
+						This card has supporting text below as a natural lead-in
+						to additional content.
+					</p>
+				</div>
+				<div class="card-footer">
+					<a href="#" class="btn btn-primary">
+						Go somewhere
+					</a>
+				</div>
+			</div>
+			<div class="card">
+				<img class="card-img-top" src="..." alt="Card image cap" />
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">
+						This is a wider card with supporting text below as a
+						natural lead-in to additional content.
+					</p>
+				</div>
+				<div class="card-footer">
+					<a href="#" class="btn btn-primary">
+						Go somewhere
+					</a>
+				</div>
 			</div>
 		</div>
 	);
--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export const Footer = () => {
 	return (
-		<blockquote className="blockquote text-center bg-dark ">
-			<footer className="blockquote-footer">
+		<blockquote className="blockquote text-center bg-dark footerH text-center">
+			<footer className="blockquote-footer ">
 				Someone famous in <cite title="Source Title">Source Title</cite>
 			</footer>
 		</blockquote>
